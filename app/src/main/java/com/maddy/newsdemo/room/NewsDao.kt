9dbdcd275a8ddc864087131b5a93895dd84ef754
Madhav Bansal <madhavbansal@Madhavs-MacBook-Air.local>
2020-05-04 15:34:08 +0530

    removed comments

--- app/src/main/java/com/maddy/newsdemo/room/NewsDao.kt
+++ app/src/main/java/com/maddy/newsdemo/room/NewsDao.kt
@@ -9,13 +9,10 @@ import com.maddy.newsdemo.model.NewsData
 @Dao
 interface NewsDao {
 
-    /* Method to insert the movies fetched from api
-     * to room */
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     @JvmSuppressWildcards
     fun insertNews(news: NewsData?):Long
 
-    /* Method to fetch the movies stored locally */
     @Query("SELECT * FROM `NewsData` order by `published_at` desc")
     fun getNews(): List<NewsData>
 
